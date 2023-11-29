a531d2c1d3b121a6fa02afa113f8d48aa78c18fb
= <=>
2023-11-29 19:34:06 +0400

    abstract factory task

---
+++ Task_/Program.cs
@@ -0,0 +1,60 @@
+﻿public interface IButton
+{
+    public void OnClick();
+    public void Render();
+}
+
+public class WindowsButton : IButton
+{
+    public void OnClick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Render()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class HTMLButton : IButton
+{
+    public void OnClick()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Render()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public abstract class Dialog
+{
+    
+    public void Render()
+    {
+        IButton ?button = CreateButton();
+        button?.OnClick();
+        button?.Render();
+    }
+
+    public virtual IButton ? CreateButton() => null;
+}
+
+public class WindowsDialog : Dialog
+{
+    public override IButton? CreateButton()
+    {
+        return new WindowsButton();
+    }
+}
+
+public class WebDialog : Dialog
+{
+    public override IButton? CreateButton()
+    {
+        return new HTMLButton(); ;
+    }
+}
